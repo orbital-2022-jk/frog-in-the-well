2a4f0a931e8038f2d3c97cf9f49ff810cf0f6429
Kim Tae Won <liberwifi@gmail.com>
2022-05-19 16:48:51 +0800

    Player Movement (Part 5)
    Update materials for bouncing off walls
    Refactor code to handle variable values in unity instead of in script
    Update movement script to use boxcast for ground check and wall check

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,60 +5,95 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D boxCol;
 
-    public bool onGround = true;
-    public float speed;
-    public int dir;
-    public float verticalInc;
+    public PhysicsMaterial2D bounceMat, normalMat;
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        Collider2D thisObject = col.collider;
+    [SerializeField] private LayerMask platform;
 
-        Vector3 contactPoint = col.contacts[0].point; // Point at which other collider intersects with this collider
-        Vector3 center = thisObject.bounds.center; // Center point of this collider to use as reference for where player is in relation to this point
+    public float dir;
+    public float charge;
+    public float chargeRate;
 
-        if ((contactPoint.y > center.y) && (col.gameObject.tag == "platform")) // for walls and platform sides
-        {
-            onGround = true;
-        }
-        else // for grounds
-        {
-            dir *= -1;
-            rb.velocity = new Vector2(dir * speed, speed);
-        }
-    }
+    // void OnCollisionEnter2D(Collision2D col)
+    // {
+    //     if (isWall())
+    //     {
+            
+    //     }
+        //     Collider2D thisObject = col.collider;
+
+        //     Vector3 contactPoint = col.contacts[0].point; // Point at which other collider intersects with this collider
+        //     Vector3 center = thisObject.bounds.center; // Center point of this collider to use as reference for where player is in relation to this point
+
+        //     if ((contactPoint.y > center.y) && (col.gameObject.tag == "platform")) // for walls and platform sides
+        //     {
+        //         onGround = true;
+        //     }
+        //     else // for grounds
+        //     {
+        //         dir *= -1;
+        //         rb.velocity = new Vector2(dir * speed, speed);
+        //     }
+    // }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump") && onGround)
+        if (Input.GetButton("Jump") && isGround())
         {
-            speed += verticalInc;
+            charge += chargeRate;
         }
 
-        if ((speed >= 30f || Input.GetButtonUp("Jump")) && onGround)
+        if ((charge >= 30f || Input.GetButtonUp("Jump")) && isGround())
         {
-            onGround = false;
-            rb.velocity = new Vector2(dir * speed, speed);
+            rb.velocity = new Vector2(dir * charge, charge);
 
-            Invoke("ResetJump", 1.0f);
+            charge = 0.0f;
         }
 
-        if (rb.velocity.magnitude == 0.0f)
+        if (!isGround() && isWall())
         {
-            onGround = true;
+            dir *= -1;
+            rb.sharedMaterial = bounceMat;
+        }
+        else
+        {
+            rb.sharedMaterial = normalMat;
         }
     }
 
-    void ResetJump()
+    private bool isGround()
     {
-        speed = 0.0f;
+        return Physics2D.BoxCast(boxCol.bounds.center, boxCol.bounds.size, 0f, Vector2.down, 0.1f, platform);
+    }
+
+    private bool isWall()
+    {
+        return Physics2D.BoxCast(boxCol.bounds.center, boxCol.bounds.size, 0f, Vector2.left, 0.01f, platform) || Physics2D.BoxCast(boxCol.bounds.center, boxCol.bounds.size, 0f, Vector2.right, 0.01f, platform);
+
+        // // 1
+        // bool wallOnleft = Physics2D.Raycast(new Vector2(
+        // transform.position.x - width, transform.position.y),
+        // -Vector2.right, rayCastLengthCheck);
+        // bool wallOnRight = Physics2D.Raycast(new Vector2(
+        // transform.position.x + width, transform.position.y),
+        // Vector2.right, rayCastLengthCheck);
+        // // 2
+        // if (wallOnleft || wallOnRight)
+        // {
+        //     return true;
+        // }
+        // else
+        // {
+        //     return false;
+        // }
     }
 }
