26176912e66f9f24cb1aa36b177c1b2e366693bc
joshuan98 <dzhei98@gmail.com>
2022-06-07 20:27:47 +0800

    Player movement (Part 13) and Player movement (Part 14)
    (Bug fix)
    Direction sometimes does not update correctly when bouncing off walls
    Attempt to fix by updating direction on collision and on each update cycle
    (Bug fix)
    Collision with ceiling sometimes changes direction
    Fixed by changing player movement script interaction with ceiling collisions

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -9,13 +9,15 @@ public class movement : MonoBehaviour
     Vector2 prev_velocity;
     private float width;
     private float height;
-    private float ray_cast_length = 0.005f;
-    private float collision_threshold = 0.5f;
+    private float ray_cast_length = 0.3f;
+    private float collision_threshold = 0.3f;
 
     public PhysicsMaterial2D bouncy_material, normal_material;
     public float dir;
     public float charge;
     public float charge_rate;
+    public float jump_height;
+    public float bounce_boost;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -30,36 +32,43 @@ public class movement : MonoBehaviour
         // contact down
         if (col_y + collision_threshold < curr_y)
         {
+            rb.sharedMaterial = normal_material;
             rb.velocity = new Vector2(0, 0);
         }
         // contact up
         else if (col_y - collision_threshold > curr_y)
         {
+            rb.velocity = new Vector2(prev_velocity.x, 0);
             // var speed = prev_velocity.magnitude;
             // var direction = Vector2.Reflect(prev_velocity.normalized, collision.contacts[0].normal);
             // rb.velocity = 0.8f * direction * Mathf.Max(speed, 0f);
         }
         else
         {
-            // contact left
-            if (col_x + collision_threshold < curr_x)
-            {
-                dir = 1;
-            }
-            // contact right
-            else if (col_x - collision_threshold > curr_x)
-            {
-                dir = -1;
-            }
+            rb.velocity = new Vector2(-prev_velocity.x, bounce_boost * prev_velocity.y);
+            // // contact left
+            // if (col_x + collision_threshold < curr_x)
+            // {
+            //     dir = 1;
+            // }
+            // // contact right
+            // else if (col_x - collision_threshold > curr_x)
+            // {
+            //     dir = -1;
+            // }
 
-            // var speed = prev_velocity.magnitude;
-            // var direction = Vector2.Reflect(prev_velocity.normalized, collision.contacts[0].normal);
-            // rb.velocity = 0.8f * direction * Mathf.Max(speed, 0f);
+            // // var speed = prev_velocity.magnitude;
+            // // var direction = Vector2.Reflect(prev_velocity.normalized, collision.contacts[0].normal);
+            // // rb.velocity = 0.8f * direction * Mathf.Max(speed, 0f);
         }
 
-        if (isGround())
+        if (isLeftWall())
         {
-            rb.sharedMaterial = normal_material;
+            dir = 1;
+        }
+        else if (isRightWall())
+        {
+            dir = -1;
         }
     }
 
@@ -92,39 +101,36 @@ public class movement : MonoBehaviour
 
         if ((charge >= 20f || Input.GetButtonUp("Jump")) && isGround())
         {
-            rb.velocity = new Vector2(dir * charge, 1.2f * charge);
+            rb.velocity = new Vector2(dir * charge, jump_height * charge);
 
             charge = 1.0f;
 
             rb.sharedMaterial = bouncy_material;
         }
 
-        if (isGround())
+        if (isLeftWall())
         {
-            if (isLeftWall())
-            {
-                dir = 1;
-            }
-            else if (isRightWall())
-            {
-                dir = -1;
-            }
+            dir = 1;
+        }
+        else if (isRightWall())
+        {
+            dir = -1;
         }
     }
 
     private bool isGround()
     {
         // return Physics2D.BoxCast(circ_col.bounds.center, circ_col.bounds.size * 0.3f, 0f, Vector2.down, 0.5f, platform);
-        return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), Vector2.down, ray_cast_length);
-        // bool left = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height), Vector2.down, ray_cast_length);
-        // bool mid = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), Vector2.down, ray_cast_length);
-        // bool right = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height), Vector2.down, ray_cast_length);
+        // return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), Vector2.down, ray_cast_length);
+        bool left = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height), Vector2.down, ray_cast_length);
+        bool mid = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), Vector2.down, ray_cast_length);
+        bool right = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height), Vector2.down, ray_cast_length);
 
-        // if (left || mid || right)
-        // {
-        //     return true;
-        // }
-        // return false;
+        if (left || mid || right)
+        {
+            return true;
+        }
+        return false;
     }
 
     private bool isLeftWall()
