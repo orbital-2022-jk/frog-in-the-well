446c3afb2ee5d46b6049e9d93769c69e0e8d9380
joshuan98 <dzhei98@gmail.com>
2022-06-23 00:06:28 +0800

    Timer (Part 2)
    - Timer updates based on time spent in game
    - Format time taken to properly display in HUD

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private static readonly string timer_pref = "Timer";
+    private float curr_time;
+    public TextMeshProUGUI curr_time_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curr_time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curr_time += Time.deltaTime;
+        PlayerPrefs.SetFloat(timer_pref, curr_time);
+        TimeSpan time_span = TimeSpan.FromSeconds(curr_time);
+        curr_time_text.text = string.Format("Time: {0:D2}:{1:D2}:{2:D2}", time_span.Hours, time_span.Minutes, time_span.Seconds);
+    }
+}
