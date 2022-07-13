7a5199be7acd38ff789faa32fc8f9bf5c69aa333
joshuan98 <dzhei98@gmail.com>
2022-07-13 20:35:33 +0800

    Map (Part 17)
    - Added Tetris stage addon feature: Tetris spawner to drop tetrominoes
    - Create Tetris spawner script
    - Tetris spawner will spawn tetris at random interval
    - Spawned tetrominoes will become non-active when it reaches the end of the Tetris stage
    - Create prefab for tetris spawner

---
+++ Assets/Scripts/TetrisSpawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisSpawner : MonoBehaviour
+{
+    public GameObject[] tetrominos = new GameObject[7];
+    public float max_delay;
+    public float min_delay;
+    private float delay;
+    private float curr_delay;
+    public float fall_speed;
+    private float x,
+        y;
+    private int random_tetromino;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+        delay = Random.Range(min_delay, max_delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curr_delay += Time.deltaTime;
+
+        if (curr_delay >= delay)
+        {
+            random_tetromino = Random.Range(0, 6);
+
+            reset_tetromino(tetrominos[random_tetromino]);
+
+            curr_delay = 0;
+            delay = Random.Range(min_delay, max_delay);
+        }
+
+        remove_tetromino();
+    }
+
+    void reset_tetromino(GameObject tetromino)
+    {
+        tetromino.SetActive(true);
+        tetromino.transform.position = new Vector3(x, y, 0);
+        tetromino.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -fall_speed);
+    }
+
+    void remove_tetromino()
+    {
+        for (int i = 0; i < tetrominos.Length; i++)
+        {
+            if (tetrominos[i].transform.position.y < 40)
+            {
+                tetrominos[i].SetActive(false);
+            }
+        }
+    }
+}
